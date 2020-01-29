b7d7ca51a02f75e4c4925ebe36a290f3b69b37b7
Akash Harapanahalli <aharapan@gatech.edu>
2020-01-29 15:06:16 -0500

    pushing before abandoning servos :(

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,32 +5,62 @@
 #include "PID.h"
 
 MotorDriver vibe;
-const uint8_t solenoid = P3_2;
 
 Servo screwHolder;
 Servo lift;
 
 Encoder encoder;
 void encoder_isr(){
+  Serial.print("hh");
   encoder.isr_();
 }
+const uint8_t screwSensor = P1_6;
 
 StepperDriver auger;
+StepperDriver screwPlacer;
 
 bool once;
 
 void grabScrews();
-uint8_t grabScrews_index = 0;
+void grabScrews_frame();
 uint32_t grabScrews_time = micros();
 
+void placeScrew();
+void placeScrew_frame();
+uint32_t placeScrew_time = micros();
+
 // Enable for a fun multithreading flex!
-bool pulse = true;
+bool pulse = false;
 uint32_t t0 = millis();
 uint32_t tt = millis();
 
-PID vibe_pid(25.0, 0.00000001  , 0.0, -255, 255);
-static int32_t vibe_position_array[10] = {0,0,0,0,0,0,0,0,0,0};
+PID vibe_pid(25.0, 0.0000000  , 0.0, -255, 255);
+int32_t vibe_position_array[10] = {0,0,0,0,0,0,0,0,0,0};
 uint16_t vibe_speed = 0;
+
+
+/**
+ * --------------------------------------------------------------UR-5 IO--------------------------------------------------------------
+ * 
+ * The UR-5 cannot communicate with the MSP-432 over digital io due to voltage thresholds. 
+ * As a result, te UR-5 will actually be using ports to communicate with the MSP.
+ * We should see virtually no difference besides requiring the read voltage to be higher than a certain number (probably associated with around 1.5V).
+ * 
+ * {
+ * When the screw is ready to be picked up, the MSP will write HIGH.
+ * When UR-5 reads HIGH, it will pick up the screw. 
+ * When UR-5 reaches a safe location, it will change states to HIGH and back to LOW.
+ * When the MSP reads FALLING, it will trigger the stepper action to put the screw safely onto the bit.
+ * The UR-5 will continue to place the screw into the jig.
+ * When the MSP reads LOW off of UR-5 and LOW off of photodiode, it will pick up a new screw.
+ * The UR-5 will wait at the safe location.
+ * }
+ * 
+ */
+bool prev_inputUR5 = 0;
+const uint8_t inputUR5  = P3_3;
+const uint8_t outputUR5 = P2_7;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,11 +68,16 @@ void setup() {
 
   vibe.begin(P3_0,P2_5);
   encoder.begin(P1_7 , encoder_isr);
-  pinMode(solenoid, OUTPUT);
+  pinMode(screwSensor, INPUT);
+  
+  pinMode(inputUR5,  INPUT_PULLUP);
+  pinMode(outputUR5, OUTPUT);
 
-  screwHolder.attach(P3_6);
+  screwHolder.attach(P3_2);
   lift.attach(P5_7);
+  
   auger.begin(P5_0, P5_2);
+  screwPlacer.begin(P5_6, P6_6);
 
   if(pulse){
     pinMode(RED_LED, OUTPUT);
@@ -57,21 +92,24 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(once){
      lift.write(40);
-     screwHolder.write(64);
-//     delayMicroseconds(1000000);
-     delay(1000);
+     screwHolder.write(106);
      auger.stepp(3200,1,500);
      
-     grabScrews();
      once = false;
-  }
-  vibe.setPower(vibe_pid.step(20 - vibe_speed) + 150);
-  auger.stepp(100, 1, 2000);
-  grabScrews_frame();
+  } 
 
-// --------
+  if(digitalRead(screwSensor) && !digitalRead(inputUR5)){
+    grabScrews();
+  }
+  
+  vibe.setPower(vibe_pid.step(20 - vibe_speed) + 175);
+  auger.stepp(100, 1, 2000);
+  
+  grabScrews_frame();
+  placeScrew_frame();
 
   auger.update();
+  screwPlacer.update();
   
   uint32_t t1 = millis();
   if((t1 - tt) >= 100){
@@ -81,7 +119,7 @@ void loop() {
     vibe_position_array[9] = encoder.get_position();
     
     vibe_speed = (vibe_position_array[9] - vibe_position_array[0]) * 1;   // ticks/s
-////    Serial.println(vibe_speed);
+    Serial.println(vibe_speed);
 
     if(pulse){
       float f = 0.00025;
@@ -100,76 +138,48 @@ void loop() {
 }
 
 void grabScrews(){
-  grabScrews_time = micros();
-  grabScrews_index = 0;
+  if((micros() - grabScrews_time) > 3200000){
+    grabScrews_time = micros();
+  }
+}
+
+void placeScrew(){
+  if((micros() - grabScrews_time) > 3200000){
+    grabScrews_time = micros();
+  }
 }
 
 
 void grabScrews_frame(){
   uint32_t t = micros() - grabScrews_time;
-  const int START_DELAY = 3000000;
-  const int HOLDER_ONE = 70;
+  const int START_DELAY = 0;
+  const int HOLDER_ONE = 135;
   if(t < START_DELAY + 1000000){
     lift.write(40);
     screwHolder.write(HOLDER_ONE);
     return;
   }
-  // i = 0
   if(t >= START_DELAY + 1000000 && t < START_DELAY + 2000000){
     lift.write(140);
-    digitalWrite(solenoid, HIGH);
     return;
   }
   if(t >= START_DELAY + 2000000 && t < START_DELAY + 2200000){
-    screwHolder.write(HOLDER_ONE + 10 + 36 * 0);
+    screwHolder.write(180);
     return;
   }
   if(t >= START_DELAY + 2200000 && t < START_DELAY + 3200000){
-    digitalWrite(solenoid, LOW);
     lift.write(40);
     return;
   }
-  if(t >= START_DELAY + 3200000 && t < START_DELAY + 4200000){
-    screwHolder.write(HOLDER_ONE + 30 + 36 * 0);
+  if(t >= START_DELAY + 3200000 && t < START_DELAY + 3450000){
+    screwHolder.write(180);
+    digitalWrite(outputUR5, HIGH);
     return;
   }
-  // i = 1
-  if(t >= START_DELAY + 4200000 && t < START_DELAY + 5200000){
-    lift.write(140);
-    digitalWrite(solenoid, HIGH);
-    return;
-  }
-  if(t >= START_DELAY + 5200000 && t < START_DELAY + 5400000){
-    screwHolder.write(HOLDER_ONE + 10 + 36 * 1);
-    return;
-  }
-  if(t >= START_DELAY + 5400000 && t < START_DELAY + 6400000){
-    digitalWrite(solenoid, LOW);
-    lift.write(40);
-    return;
-  }
-  if(t >= START_DELAY + 6400000 && t < START_DELAY + 7400000){
-    screwHolder.write(HOLDER_ONE + 30 + 36 * 1);
-    return;
-  }
-  // i = 2
-  if(t >= START_DELAY + 7400000 && t < START_DELAY + 8400000){
-    lift.write(140);
-    digitalWrite(solenoid, HIGH);
-    return;
-  }
-  if(t >= START_DELAY + 8400000 && t < START_DELAY + 8600000){
-    screwHolder.write(HOLDER_ONE + 10 + 34 * 2);
-    return;
-  }
-  if(t >= START_DELAY + 8600000 && t < START_DELAY + 9600000){
-    digitalWrite(solenoid, LOW);
-    lift.write(40);
-    return;
-  }
-  if(t >= 9600000 && t < 10600000){
-    screwHolder.write(HOLDER_ONE + 30 + 34 * 2);
-    return;
-  }
-  screwHolder.write(180);
+  return;
+}
+
+
+void placeScrew_frame(){
+
 }
